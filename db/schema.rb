d830a2c44702a935241600b1118fa491cf8ffd67
nepttako <nepttako@gmail.com>
2016-06-01 14:42:31 +0800

    add user table and relation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601062406) do
+ActiveRecord::Schema.define(version: 20160601063803) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20160601062406) do
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
