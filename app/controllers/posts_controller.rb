182f56a5abaa7e2e3d5f36f1c9700ddee6fa321f
nepttako <nepttako@gmail.com>
2016-06-30 18:09:55 +0800

    add categories to index and post views. add category checkboxes in new post view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
     @users = User.all
+
   end
 
   # GET /posts/1
@@ -75,6 +76,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :user_id)
+      params.require(:post).permit(:title, :content, category_ids: [])
     end
 end
